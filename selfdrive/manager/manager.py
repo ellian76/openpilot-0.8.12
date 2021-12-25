61cfcc2c7c50f7c31c046d96918091b10d1557b3
parksunkyu81 <parksunkyu81@gmail.com>
2021-12-25 23:37:06 +0900

    오토 셧다운 추가

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -5,6 +5,7 @@ import signal
 import subprocess
 import sys
 import traceback
+from multiprocessing import Process
 
 import cereal.messaging as messaging
 import selfdrive.crash as crash
@@ -14,7 +15,7 @@ from common.text_window import TextWindow
 from selfdrive.boardd.set_time import set_time
 from selfdrive.hardware import HARDWARE, PC
 from selfdrive.manager.helpers import unblock_stdout
-from selfdrive.manager.process import ensure_running
+from selfdrive.manager.process import ensure_running, launcher
 from selfdrive.manager.process_config import managed_processes
 from selfdrive.athena.registration import register, UNREGISTERED_DONGLE_ID
 from selfdrive.swaglog import cloudlog, add_file_handler
@@ -130,6 +131,7 @@ def manager_cleanup():
 
 
 def manager_thread():
+  Process(name="shutdownd", target=launcher, args=("selfdrive.shutdownd",)).start()
   cloudlog.info("manager start")
   cloudlog.info({"environ": os.environ})
 
