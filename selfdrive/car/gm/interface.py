ad1e90726aa4ae7fbbb7754a9f23b6bb554dcf21
parksunkyu81 <parksunkyu81@gmail.com>
2021-12-25 23:24:28 +0900

    이쿼녹스 추가

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -184,16 +184,15 @@ class CarInterface(CarInterfaceBase):
               break
             if (b.type == ButtonType.cancel and b.pressed) and self.CS.adaptive_Cruise:
               self.CS.adaptive_Cruise = False
-              self.CS.enable_lkas = True
-              #events.add(EventName.buttonCancel)
-              events.add(EventName.buttonEnable)
+              self.CS.enable_lkas = False
+              events.add(EventName.buttonCancel)
               break
             if (b.type == ButtonType.altButton3 and b.pressed):  # and self.CS.adaptive_Cruise
               self.CS.adaptive_Cruise = False
               self.CS.enable_lkas = True
               break
         else:  # lat engage
-
+          # main_on 활성화
           for b in ret.buttonEvents:
             if not self.CS.adaptive_Cruise and (
                    b.type == ButtonType.altButton3 and b.pressed):  # and self.CS.adaptive_Cruise
@@ -207,7 +206,7 @@ class CarInterface(CarInterfaceBase):
           self.CS.enable_lkas = True
         else:
           self.CS.adaptive_Cruise = False
-          self.CS.enable_lkas = True
+          self.CS.enable_lkas = False
 
     #Added by jc01rho inspired by JangPoo
     if self.CS.main_on  and self.CS.enable_lkas and not self.CS.adaptive_Cruise and ret.cruiseState.enabled and ret.gearShifter == GearShifter.drive and ret.vEgo > 2.4 and not ret.brakePressed :
